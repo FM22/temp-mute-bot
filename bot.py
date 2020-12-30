705eca7f1ceb92bbe4074c170060e2ecf97a1e31
FM22 <artem.khovanov@gmail.com>
2020-12-30 18:10:48 +0000

    More improvements

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ admin_roles = {"super admin", "artem", "nice person"}
 msgDict = {"mute": "muted", "blind": "blinded"}
 admin_commands = {"admin": admin_roles, "channel": bound_channels}
 admin_commands_txt = {"admin": "as an admin role", "channel": "as a bound channel"}
+max_time = 86,400
 
 
 # would really love to avoid having to do this
@@ -30,6 +31,7 @@ async def on_message(message):
     author = message.author
     channel = message.channel
     is_admin = intersection(set([r.name for r in author.roles]), admin_roles) or author.permissions_in(channel).administrator
+    admin_fail = False
 
     # help command
     if not (author.bot or author.system) and message.content == ';help':
@@ -44,10 +46,18 @@ async def on_message(message):
         *Admin only commands:*\n
         **;clear [@user1] [@user2] ...**\n
         Unmutes and unblinds specified users.\n\n
-        **;admin [add/remove] [role]**\n
+        **;admin [add/remove] [role name]**\n
         Adds or removes the specified role as an admin role for this bot.\n\n
+        **;admin display**\n
+        Displays the current admin roles.\n\n
         **;channel [add/remove] [channel name]**\n
-        Adds or removes the specified channel as a usable channel for the mute and blind commands.""")
+        Adds or removes the specified channel as a usable channel for the mute and blind commands\n\n
+        **;channel display**\n
+        Displays the current bound channels.\n\n
+        **;maxtime [set] [time][s/m/h]**\n
+        Sets the maximum mute/blind time.
+        **;maxtime display**\n
+        Displays the current maximum mute/blind time.\n\n""")
         return
 
     # other commands
@@ -61,17 +71,14 @@ async def on_message(message):
             # mute/blind command
             if words[0] in roleDict.keys():
                 newRole = roleDict[words[0]]
-                base = words[1][:-1]
-                if base.isdigit():
-                    multi = words[1][-1]
-                    timeout = timeDict.get(multi, 60) * int(base)
-                    await author.add_roles(newRole)
-                    await channel.send(msgDict[words[0]].title() + " " + author.name + " for " + str(int(base)) + (multi if multi in timeDict.keys() else 'm'))
-                    await asyncio.sleep(timeout)
-                    if newRole in discord.utils.get(client.get_all_members(), id = author.id).roles:
-                        await author.remove_roles(newRole)
-                        await channel.send(author.name + " is back!")
-                    return
+                time = getTimeVal(words[1])
+                await author.add_roles(newRole)
+                await channel.send(msgDict[words[0]].title() + " " + author.name + " for " + getTimeStr(time) + " " + (multi if multi in timeDict.keys() else 'm'))
+                await asyncio.sleep(time)
+                if newRole in discord.utils.get(client.get_all_members(), id = author.id).roles:
+                    await author.remove_roles(newRole)
+                    await channel.send(author.name + " is back!")
+                return
 
             # admin-only commands
             if is_admin:
@@ -91,7 +98,37 @@ async def on_message(message):
                     elif words[1] == "remove":
                         admin_commands[words[0]].remove(words[2])
                         await channel.send("Removed " + words[2] + " " + admin_commands_txt[words[0]])
+                    elif words[1] == "display":
+                        await channel.send([o.name for o in admin_commands[words[0]]])
+                    return
+
+                if words[0] == "maxtime":
+                    if words[1] == "display":
+                        await channel.send("Maximum time is " + getTimeStr(max_time))
+                    elif words[1] == "set":
+                        new_time = getTimeVal(words[2])
+                        if new_time != -1:
+                            max_time = new_time
+                            await channel.send("Set max time to " +getTimeStr(words[2]))
             else:
                 await author.send("Get an admin to do this :slight_smile:")
+
+# converts time string into numerical value (-1 if it fails)
+def getTimeVal(timeStr):
+    # need to improve this
+    if len(timeStr) > 1:
+        base = timeStr[:-1]
+        if base.isdigit():
+            multi = timeStr[-1]
+            timeout = timeDict.get(multi, 60) * int(base)
+            timeout = min(timeout, max_time)
+    return -1
+
+# converts number of seconds into time string
+def getTimeStr(time):
+    secs = time % 60
+    mins = floor(time/60) % 60
+    hrs = floor(time/3600)
+    return str(hrs) + "h" + str(mins) + "m" + str(secs) + "s"
 
 client.run(TOKEN)
